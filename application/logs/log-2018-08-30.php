65408491acdaeb714f74ec6dea6a36b87b2fc2a2
Lenovo-PC\user <ahmad.sahro@cartenz.co.id>
2018-08-30 09:21:20 +0700

    no message

--- application/logs/log-2018-08-30.php
+++ application/logs/log-2018-08-30.php
@@ -2,3 +2,5 @@
 
 ERROR - 2018-08-30 03:44:18 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: NO) C:\xampp\htdocs\eradise\system\database\drivers\mysqli\mysqli_driver.php 161
 ERROR - 2018-08-30 03:44:18 --> Unable to connect to the database
+ERROR - 2018-08-30 03:56:10 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: NO) C:\xampp\htdocs\eradise\system\database\drivers\mysqli\mysqli_driver.php 161
+ERROR - 2018-08-30 03:56:10 --> Unable to connect to the database
